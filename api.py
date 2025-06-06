72ed7bde812a784ad3555428806e6218aa1fe9ec
davidatobs <davidatobatele2@gmail.com.com>
2025-06-06 13:06:59 -0400

    Added CORS support to flask API

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import re
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load model and vectorizer
 model = joblib.load("waf_model.pkl")
